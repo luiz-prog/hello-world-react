830f7bec7f2f9c6b6390333bb620165dd29ca5b6
luiz-prog <111691274+luiz-prog@users.noreply.github.com>
2025-04-07 19:47:23 -0300

    init

---
+++ src/hello-world/HelloWorld.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+
+export default function HelloWorld(){
+    let [name, setName] = useState('');
+    let[message, setMessage] = useState('');
+
+    const handleHelloWorld = () => setMessage(`Hello World ${name}!`);
+
+    function handleClear() {
+        setName('');
+        setMessage('');
+    };
+
+    const handleChangeInput = (event) => setName(event.target.value)
+
+    const handleKeyInput = (event) => {
+        console.log(event);
+        if (event.key === 'Escape') {
+            handleClear();
+        }else if(event.key ==='Enter') { handleHelloWorld()}; 
+        
+    };
+
+    return (
+        <>
+            <h1>Hello World!</h1>
+            <input type="text" onChange={handleChangeInput} onKeyUp={handleKeyInput} value={name} />
+            <button onClick={handleHelloWorld}>Hello World</button>
+            <button onClick={handleClear}>Limpar</button>
+            {message && <p>{message}</p>}
+        </>
+    )
+}
+
+
